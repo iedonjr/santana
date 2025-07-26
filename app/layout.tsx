b479a577d8948f87ab1afafef57f5c53575ad045
santana <hiedonjr@gmail.com>
2025-07-26 06:24:18 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,24 +15,27 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <head>
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5FD5SRP2');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5FD5SRP2');`,
           }}
         />
-        {/* End Google Tag Manager */}
 
-        {/* Event snippet for Lead-Whats conversion page */}
+        {/* Google Ads Conversion - Lead WhatsApp */}
         <script
           dangerouslySetInnerHTML={{
             __html: `gtag('event', 'conversion', {
-            'send_to': 'AW-17355075473/xtWnCMSbi_IaEJHfxdNA',
-            'value': 1.0,
-            'currency': 'BRL'
-          });`,
+              send_to: 'AW-17355075473/xtWnCMSbi_IaEJHfxdNA',
+              value: 1.0,
+              currency: 'BRL'
+            });`,
           }}
         />
       </head>
@@ -44,7 +47,7 @@ export default function RootLayout({
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          />
+          ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
         {children}
